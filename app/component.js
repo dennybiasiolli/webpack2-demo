e72ccf48b064f52cad60671e19a1b428b5032f3d
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2017-01-03 10:28:14 +0100

    first sample code with old style using of code

---
+++ app/component.js
@@ -0,0 +1,10 @@
+function component() {
+  var element = document.createElement('div');
+
+  /* lodash is required for the next line to work */
+  element.innerHTML = ['Hello', 'webpack'].map(function(item) {
+    return item + ' ';
+  });
+
+  return element;
+}
