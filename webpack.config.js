0f357d1335ea9b049937452c3a14a4d22fdddbfb
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2017-01-03 11:04:31 +0100

    webpack: adding babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['latest']
+        }
+      }
+    ]
   }
 }
